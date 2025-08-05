285b6d822e86418b7d4d5c603d9ccae22cb48010
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-08-05 11:39:42 +0800

    恢复现代的panda最大扭矩限制

--- board/safety/safety_hyundai.h
+++ board/safety/safety_hyundai.h
@@ -17,8 +17,8 @@
   .has_steer_req_tolerance = true, \
 }
 
-const SteeringLimits HYUNDAI_STEERING_LIMITS = HYUNDAI_LIMITS(512, 10, 10);
-const SteeringLimits HYUNDAI_STEERING_LIMITS_ALT = HYUNDAI_LIMITS(512, 10, 10);
+const SteeringLimits HYUNDAI_STEERING_LIMITS = HYUNDAI_LIMITS(384, 3, 7);
+const SteeringLimits HYUNDAI_STEERING_LIMITS_ALT = HYUNDAI_LIMITS(270, 2, 3);
 
 const LongitudinalLimits HYUNDAI_LONG_LIMITS = {
   .max_accel = 200,   // 1/100 m/s2
